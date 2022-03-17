9a40ea8355c0aad6e8050143952e3eb5faa542b0
Maria Markina <rbsk@bk.ru>
2022-03-17 13:26:27 +0300

    Add junit4

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,48 +1,39 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import junit.framework.TestCase;
+
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateRemainWhenAmount1500() {
-        int actual = service.remain(1500);
         int expected = 500;
+        int actual = service.remain(1500);
 
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    public void shouldCalculateRemainWhenAmount900() {
-        int actual = service.remain(900);
-        int expected = 100;
-
-        assertEquals(actual, expected);
-    }
-
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateRemainWhenAmount999() {
-        int actual = service.remain(999);
         int expected = 1;
-
-        assertEquals(actual, expected);
+        int actual = service.remain(999);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldCalculateRemainWhenAmount1001() {
-        int actual = service.remain(1001);
         int expected = 999;
-
-        assertEquals(actual, expected);
+        int actual = service.remain(1001);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
-    public void shouldCalculateRemainWhenAmount1000() {   // баг (не должен напоминать (т.е. 0),
-        int actual = service.remain(1000);        // а предлагает докупить на еще 1000
-        int expected = 1000;
-
-        assertEquals(actual, expected);
+    @org.junit.Test
+    public void shouldCalculateRemainWhenAmount1000() {  // баг (не должен напоминать (т.е. 0),
+        int expected = 1000;                             // а предлагает докупить на еще 1000
+        int actual = service.remain(1000);
+        assertEquals(expected, actual);
     }
 }
 
