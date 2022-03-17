d7c4d1adb47db3aebeab244fafb27d3fadf4d231
Maria Markina <rbsk@bk.ru>
2022-03-17 13:04:10 +0300

    Add testng

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,48 @@
 package ru.netology.service;
 
-import junit.framework.TestCase;
-import org.junit.Test;
+import static org.testng.Assert.*;
 
-public class CashbackHackServiceTest extends TestCase {
+public class CashbackHackServiceTest {
+    CashbackHackService service = new CashbackHackService();
 
+    @org.testng.annotations.Test
+    public void shouldCalculateRemainWhenAmount1500() {
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateRemainWhenAmount900() {
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateRemainWhenAmount999() {
+        int actual = service.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateRemainWhenAmount1001() {
+        int actual = service.remain(1001);
+        int expected = 999;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void shouldCalculateRemainWhenAmount1000() {   // баг (не должен напоминать (т.е. 0),
+        int actual = service.remain(1000);        // а предлагает докупить на еще 1000
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
 }
+
